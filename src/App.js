996f7781c73485d4828341209e5e058378c58118
vladimir greenfield <vladdygreenfield@gmail.com>
2021-12-17 14:48:01 -0500

    Created the starter code for the pages that will ne in the app and rendered them to the wed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
-
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Appointments from './Pages/Appointments';
+import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+import Register from './Pages/Register';
 class App extends Component {
   render() {
   return (
     <div className="App">
         <h1>App</h1>
+        <Home />
+        <About />
+        <Appointments />
+        <Login />
+        <Profile />
+        <Register />
     </div>
   )};
 }
---
+++ src/Pages/Appointments.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Appointments() {
+    return(
+        <div>Appontments</div>
+    );
+}
+
+export default Appointments;
